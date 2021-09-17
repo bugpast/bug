19230a5b1b4e067958fc7ca41aac93daf8a87622
bugpast <90888016+bugpast@users.noreply.github.com>
2021-09-17 13:44:40 +0800

    Update myname.c

--- myname.c
+++ myname.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-
-main() {
-printf("曹竣崴\n");
-}
-
+# National Hsinchu Senior Industrial Vocational School
+## Programming, Introduction of Computer
+### 3rd grade
+- langugage: C
+- topic: stdio
